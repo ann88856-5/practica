5964967b19de11cd759256b251e78ac0c8e4c636
Ann Tkacheva <anna2006tk@gmail.com>
2025-06-27 20:37:44 +0700

    feat: реализован просмотр отдельных цветовых каналов

--- app.py
+++ app.py
@@ -12,21 +12,15 @@ class ImageApp:
         self.btn_load.pack(pady=10)
 
         self.channel_var = tk.StringVar(value="R")
-        for text, val in [("Красный", "R"), ("Зелёный", "G"), ("Синий", "B")]:
-            rb = tk.Radiobutton(master, text=text, variable=self.channel_var, value=val, state='disabled')
-            rb.pack(anchor="w")
+        self.rb_red = tk.Radiobutton(master, text="Красный", variable=self.channel_var, value="R", state='disabled', command=self.update_channel)
+        self.rb_green = tk.Radiobutton(master, text="Зелёный", variable=self.channel_var, value="G", state='disabled', command=self.update_channel)
+        self.rb_blue = tk.Radiobutton(master, text="Синий", variable=self.channel_var, value="B", state='disabled', command=self.update_channel)
+        self.rb_red.pack(anchor="w")
+        self.rb_green.pack(anchor="w")
+        self.rb_blue.pack(anchor="w")
 
         self.img_label = tk.Label(master)
         self.img_label.pack(pady=5)
-
-        self.btn_line = tk.Button(master, text="Нарисовать линию", state='disabled')
-        self.btn_line.pack(pady=5)
-
-        self.btn_rotate = tk.Button(master, text="Вращение", state='disabled')
-        self.btn_rotate.pack(pady=5)
-
-        self.btn_resize = tk.Button(master, text="Изменить размер", state='disabled')
-        self.btn_resize.pack(pady=5)
 
         self.image = None
         self.tk_image = None
@@ -36,16 +30,35 @@ class ImageApp:
             filetypes=[("Изображения", "*.png *.jpg *.jpeg *.bmp *.gif"), ("Все файлы", "*.*")]
         )
         if path:
-            self.image = Image.open(path)
-            self.show_image(self.image)
-            for rb in self.root.winfo_children():
-                if isinstance(rb, tk.Radiobutton):
-                    rb.config(state='normal')
+            self.image = Image.open(path).convert("RGB")  
+            self.update_channel()
+            for rb in (self.rb_red, self.rb_green, self.rb_blue):
+                rb.config(state='normal')
+
+    def get_channel_image(self):
+        if self.image is None:
+            return None
+        # Разделяем каналы
+        r, g, b = self.image.split()
+        channel = self.channel_var.get()
+        if channel == "R":
+            return r
+        elif channel == "G":
+            return g
+        elif channel == "B":
+            return b
+        else:
+            return self.image
+
+    def update_channel(self):
+        channel_img = self.get_channel_image()
+        if channel_img:
+            self.show_image(channel_img)
 
     def show_image(self, img):
         self.tk_image = ImageTk.PhotoImage(img)
         self.img_label.config(image=self.tk_image)
-        self.img_label.image = self.tk_image  
+        self.img_label.image = self.tk_image
 
 
 if __name__ == "__main__":
